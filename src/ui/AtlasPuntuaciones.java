933a70d83694f019eef5bbe29a5cecb57f4cacaf
Laura Sofia Guerrero Ante <1149934564@u.icesi.edu.co>
2025-03-07 12:59:05 -0500

    Seg2

--- src/ui/AtlasPuntuaciones.java
+++ src/ui/AtlasPuntuaciones.java
@@ -15,7 +15,13 @@ public class AtlasPuntuaciones {
 	private Scanner escaner;
 
 	// Declaración de nuestras constantes a utilizar
-
+	 	private final int MINIMO_CANTIDAD_JUGADORES=1;
+		private final int MAXIMO_CANTIDAD_JUGADORES =10;
+	  
+	  private final int MINIMORONDAS =1;
+	  private final int MAXIMORONDAS =5;
+      private final int MINIMOPUNTAJE =0;
+      private final int MAXIMOPUNTAJE =100;
 
 	private AtlasPuntuaciones() {
 		escaner = new Scanner(System.in);
@@ -24,23 +30,54 @@ public class AtlasPuntuaciones {
 	
 	public void run()
 	{
+
+			int numerojugadores;
+			int numeroderondas;
+
 		// Declaracion de todas las variables a usar en el programa
+		
+		// aqui en este paso solicitaremos la cantidad de jugadores 
+		 
+		  System.out.println ("Holaa, ingresame la cantidad de jugadores que deseas :3");
+		  numerojugadores = escaner.nextInt();
+		  escaner.nextLine();
+		      System.out.println ("Holaa, ingresame tambien el numero rondas que deseas :3");
 
+          numeroderondas = escaner.nextInt();
+		  escaner.nextLine();
+		  
+           System.out.println ("Holaa, ingresame tambien el numero rondas que deseas :3");
 
-		// Notificacion al usuario de la solicitud de dato
+			/*
+		   MINIMO_CANTIDAD_JUGADORES + "-" +
+		   MAXIMO_CANTIDAD_JUGADORES +"):");
+		   */
+			if(numerojugadores<MINIMO_CANTIDAD_JUGADORES && numerojugadores>MAXIMO_CANTIDAD_JUGADORES){
 
-		// Capturamos el dato con nuestro Scanner (que se llama, escaner)
+				System.out.println ("no es un valor ");
 
+			}
+              while(numerojugadores<MINIMO_CANTIDAD_JUGADORES && numerojugadores>MAXIMO_CANTIDAD_JUGADORES);
+		    /* solicitamos la cantidad de rondas en esta area 
+            */ 
+            System.out.println ("Ingresa por fis la cantidad de rondas"+MINIMORONDAS + MAXIMORONDAS);
+			  numeroderondas = escaner.nextInt();
+			  if(numeroderondas<MINIMORONDAS && numeroderondas>MAXIMORONDAS);
+              
+            System.out.println ("Este valor no es valido D:"); 
 
+			  while(numeroderondas<minimorondas && numerorondas>maximorondas);{
 
-		// Realizamos los cálculos delegandolos a los métodos
+			  }
+			  /*
+			  usemos arreglos para poner los nombres como tambien los puntajes que nos piden 
 
+			  */
 
-		// Notificación al usuario de los resultados
-
-
+			 String[][] nombres= new String[numerojugadores][numeroderondas];
 	}
-
+			  
+                
 	public static void main(String[] args) {
 		AtlasPuntuaciones mainApp = new AtlasPuntuaciones();
 		mainApp.run();
@@ -52,7 +89,7 @@ public class AtlasPuntuaciones {
 	 * @return 
 	 */
     public int calcularSumaTotal(int[] numeros) {
-        // Completar
+       return 0;
     }
 
 	/**
@@ -61,7 +98,7 @@ public class AtlasPuntuaciones {
 	 * @return 
 	 */
     public double calcularPromedio(int[] numeros) {
-        // Completar
+        return 0;
     }
 
 	/**
@@ -70,7 +107,7 @@ public class AtlasPuntuaciones {
 	 * @return 
 	 */
     public double encontrarMayor(double[] numeros) {
-        // Completar
+        return 0;
     }
 
 	
